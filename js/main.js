f1b97bd7f9cfe97689171f544591ee33d3ade101
Barbara Stadnik <barbara1stadnik@gmail.com>
2018-06-10 12:31:05 +0200

    dragging mechanism - only bar is dragged

---
+++ js/main.js
@@ -0,0 +1,41 @@
+(function() {
+	'use strict';
+
+	var draggedEl,
+		onDragStart,
+		onDrag,
+		onDragEnd,
+		grabPointY,
+		grabPointX;
+
+	onDragStart = function(ev) {
+		var boundingClientRect;
+
+		if (ev.target.className.indexOf('bar') === -1) {
+			return;
+		}
+
+		draggedEl = this;
+
+		boundingClientRect = draggedEl.getBoundingClientRect();
+
+		grabPointY = boundingClientRect.top - ev.clientY;
+		grabPointX = boundingClientRect.left - ev.clientX;
+	};
+
+	onDrag = function(ev) {
+		if (!draggedEl) {
+			return;
+		}
+
+		var posX = ev.clientX + grabPointX,
+			posY = ev.clientY + grabPointY;
+
+		draggedEl.style.transform = "translateX(" + posX + "px) translateY(" + posY + "px)";
+	}
+
+	document.addEventListener('mousemove', onDrag, false);
+	document.querySelector('.sticker .bar').addEventListener('mousedown',
+		onDragStart, false);
+
+})();
